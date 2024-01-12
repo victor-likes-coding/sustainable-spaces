65a8a0380d42705d449867189f543412be36a3a1
Victor Tran <victor-likes-coding@tempestas.io>
2024-01-12 03:17:54 -0500

    create landing / cta

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -8,5 +8,20 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  return <div></div>;
+  return (
+    <div className="w-full h-without-nav bg-primary text-white">
+      <main className="px-16 h-full flex justify-center align-middle flex-col text-center">
+        <h1 className="text-3xl mb-8">
+          Welcome to{" "}
+          <span className="font-bold text-secondary">Sustainable Spaces</span>
+        </h1>
+        <button
+          type="button"
+          className="bg-green-700 font-bold px-2 py-2 rounded-full mx-12"
+        >
+          Get Started
+        </button>
+      </main>
+    </div>
+  );
 }
