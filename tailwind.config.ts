d3cd8c878a9f44ad4c6168570a8cca1e09463b0f
Victor Tran <victor-likes-coding@tempestas.io>
2024-01-12 03:14:53 -0500

    add theme configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,16 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#79abaf",
+        secondary: "#7C9952",
+        tertiary: "#021808",
+      },
+      height: {
+        "without-nav": "calc(100vh - 3rem)",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
