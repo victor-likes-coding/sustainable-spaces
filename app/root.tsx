a109b40d5b80a8d73a4c834a14796d492b8cd0ad
Victor Tran <victor-likes-coding@tempestas.io>
2024-01-12 04:46:11 -0500

    add footer

--- app/root.tsx
+++ app/root.tsx
@@ -41,6 +41,41 @@ export default function App() {
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
+        <footer className="bg-dark text-white p-4 text-[.6rem] px-8">
+          <nav className="flex justify-between flex-wrap">
+            <div className="col-wrapper flex flex-col gap-2">
+              <div>Useful Links</div>
+              <Link to="/">Home</Link>
+              <Link to="/about">About</Link>
+              <Link to="/services">Services</Link>
+              <Link to="/privacy">
+                Privacy <span className="hidden">Policy</span>
+              </Link>
+              <Link to="/tos">
+                Terms <span className="hidden">and Conditions</span>
+              </Link>
+            </div>
+            <div className="col-wrapper flex flex-col gap-2">
+              <div>Community</div>
+              <Link to="/help">Help</Link>
+              <Link to="/properties">Properties</Link>
+              <Link to="/forum">Forum</Link>
+              <Link to="/support">Support</Link>
+            </div>
+
+            <div className="col-wrapper flex flex-col gap-2">
+              <div>Contact</div>
+              <Link to="mailto:some-email@email.com" target="__blank">
+                some-email@email.com
+              </Link>
+              <Link to="tel:+1-800-123-4567" target="__blank">
+                +1 (800) 123-4567
+              </Link>
+              <div>New York, NY 10012, US</div>
+            </div>
+            <div className="flex w-full justify-around"></div>
+          </nav>
+        </footer>
       </body>
     </html>
   );
