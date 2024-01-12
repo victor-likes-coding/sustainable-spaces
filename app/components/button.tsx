3078ec5eae49d9f237b435492dbc7bd984969d43
Victor Tran <victor-likes-coding@tempestas.io>
2024-01-12 18:34:54 -0500

    update button to return a link or button

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -1,16 +1,27 @@
+import { Link } from "@remix-run/react";
+
 type Props = {
   text: string;
   className?: string;
+  link?: boolean;
+  to?: string;
 };
 
-const Button = ({ text, className }: Props) => {
-  return (
+const Button = ({ text, className, to, link }: Props) => {
+  return !link ? (
     <button
       type="button"
-      className={`bg-green-700 font-bold px-2 py-2 rounded-full mx-12 ${className}`.trim()}
+      className={`bg-green-700 font-bold px-2 py-2 rounded-full mx-2 small:mx-10 med:mx-14 large:w-[30%] large:mx-auto ${className}`.trim()}
     >
       {text}
     </button>
+  ) : (
+    <Link
+      to={to ? to : ""}
+      className={`bg-green-700 font-bold px-2 py-2 rounded-full mx-2 small:mx-10 med:mx-14 large:w-[30%] large:mx-auto ${className}`.trim()}
+    >
+      {text}
+    </Link>
   );
 };
 
