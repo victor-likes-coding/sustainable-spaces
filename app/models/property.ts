669cfdd2f2804e1d36555800684a1894dd80f8c1
Victor Tran <victor-likes-coding@tempestas.io>
2024-01-13 16:01:43 -0500

    Refactor Property class and add PropertyService interface

--- app/models/property.ts
+++ app/models/property.ts
@@ -1,65 +1,76 @@
-import { PropertyData } from "./property.d";
+import { FullPropertyData, BasicPropertyData } from "./property.d";
 import { User } from "./user";
 
-interface iProperty {
-  id: string;
+export class Property implements FullPropertyData {
+  id: number;
   address: string;
-  zip: string;
   city: string;
-  unit: string;
-  longitude: string;
-  latitude: string;
-  balance: number;
-  capExFee: number;
-  managementFee: number;
+  state: string;
+  zip: number;
+  price: number;
+  beds: number;
+  baths: number;
+  sqft: number;
+  tax: number;
+  hoa: number;
+  yearBuilt: number;
+  management: number;
+  insurance: number;
+  capex: number;
+  vacancy: number;
+  updated: Date;
+  created: Date;
   rent: number;
-  owner: User;
-  renter: User;
-}
-export class Property implements iProperty {
-  owner: User;
-  renter: User;
-  id: string;
-  address: string;
-  zip: string;
-  city: string;
-  unit: string;
-  longitude: string;
-  latitude: string;
-  balance: number;
-  capExFee: number;
-  managementFee: number;
-  rent: number;
+  payment: number;
+  garage: number;
+  owner: string | User;
+  tenant: string | User;
+  likes: number[];
+  likesCount: number;
+  longitude: number;
+  latitude: number;
+  description: string;
+  allowRentOption: boolean;
 
-  constructor({
-    owner,
-    renter,
-    id,
-    address,
-    zip,
-    city,
-    unit,
-    longitude,
-    latitude,
-    balance,
-    capExFee,
-    managementFee,
-    rent,
-  }: iProperty) {
-    this.owner = owner;
-    this.renter = renter;
-    this.id = id;
-    this.address = address;
-    this.zip = zip;
-    this.city = city;
-    this.unit = unit;
-    this.longitude = longitude;
-    this.latitude = latitude;
-    this.balance = balance;
-    this.capExFee = capExFee;
-    this.managementFee = managementFee;
-    this.rent = rent;
+  constructor(data: FullPropertyData) {
+    this.id = data.id;
+    this.address = data.address;
+    this.city = data.city;
+    this.state = data.state;
+    this.zip = data.zip;
+    this.price = data.price;
+    this.beds = data.beds;
+    this.baths = data.baths;
+    this.sqft = data.sqft;
+    this.tax = data.tax;
+    this.hoa = data.hoa;
+    this.yearBuilt = data.yearBuilt;
+    this.management = data.management;
+    this.insurance = data.insurance;
+    this.capex = data.capex;
+    this.vacancy = data.vacancy;
+    this.updated = data.updated;
+    this.created = data.created;
+    this.rent = data.rent;
+    this.payment = data.payment;
+    this.garage = data.garage;
+    this.owner = data.owner;
+    this.tenant = data.tenant;
+    this.likes = data.likes;
+    this.likesCount = data.likesCount;
+    this.longitude = data.longitude;
+    this.latitude = data.latitude;
+    this.description = data.description;
+    this.allowRentOption = data.allowRentOption;
   }
 }
 
-export type { PropertyData };
+export abstract class PropertyService {
+  abstract getProperties(): Promise<Property[]>;
+  abstract getProperty(id: number): Promise<Property>;
+  abstract createProperty(property: Property): Promise<Property>;
+  abstract updateProperty(property: Property): Promise<Property>;
+  abstract deleteProperty(id: number): Promise<void>;
+}
+
+export type { FullPropertyData, BasicPropertyData };
