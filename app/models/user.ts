810912db1e9d83bf8658cebeaa9450227c8cac88
Victor Tran <victor-likes-coding@tempestas.io>
2024-01-13 00:58:26 -0500

    work on creating UserService methods

--- app/models/user.ts
+++ app/models/user.ts
@@ -2,6 +2,7 @@ import { userAuthData } from "./user.d";
 // import { Property } from "./property";
 import { Bank } from "./bank";
 import { validateAuth } from "~/utils/helper";
+import { PasswordService } from "./password";
 
 interface iUser {
   id?: string;
@@ -22,13 +23,26 @@ export class User implements iUser {
 }
 
 export abstract class UserService {
-  createUser(data: userAuthData) {
+  public static async createUser(data: userAuthData) {
     // validate userAuthData
     if (validateAuth(data)) {
       // hash password
+      const hash = await PasswordService.hashPassword({
+        password: data.password,
+        saltRounds: 10,
+      });
       // store data into database
+      const user = UserService.createModel(data, hash);
+
       // return newly created user
     }
+  }
+
+  private static createModel({ email }: userAuthData, hash: string) {
+    return {
+      email,
+      password: hash,
+    };
   }
 }
 export type { userAuthData };
