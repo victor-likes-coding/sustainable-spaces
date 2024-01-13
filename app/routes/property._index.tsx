5d29c6debe373be354a9bd3e7c159011e502e5aa
Victor Tran <victor-likes-coding@tempestas.io>
2024-01-13 18:18:28 -0500

    Refactor Property component and use actual data from loader

--- app/routes/property._index.tsx
+++ app/routes/property._index.tsx
@@ -1,7 +1,7 @@
 // import { Form, Link } from "@remix-run/react";
 
 import { json } from "@remix-run/node";
-// import { useLoaderData } from "@remix-run/react";
+import { useLoaderData } from "@remix-run/react";
 import PropertyCard from "~/components/propertycard";
 import { FullPropertyData } from "~/models/property";
 import { db } from "~/utils/db.server";
@@ -14,50 +14,18 @@ export const loader = async () => {
   });
 };
 
-const fakeData: FullPropertyData[] = [
-  {
-    id: 1,
-    address: "1234 Main St",
-    city: "San Francisco",
-    state: "CA",
-    zip: 94111,
-    price: 300000,
-    beds: 3,
-    baths: 2,
-    sqft: 2000,
-    tax: 3400,
-    hoa: 1200,
-    yearBuilt: 2000,
-    management: 2400,
-    insurance: 1200,
-    capex: 1200,
-    vacancy: 1200,
-    updated: new Date(),
-    created: new Date(),
-    rent: 3000,
-    payment: 1000,
-    garage: 2,
-    owner: "1",
-    tenant: "2",
-    likes: [],
-    likesCount: 0,
-    longitude: 0,
-    latitude: 0,
-    description: "",
-    allowRentOption: true,
-  },
-];
-
 export default function Property() {
-  // const properties = useLoaderData<typeof loader>;
-  const property = fakeData[0];
+  const properties = useLoaderData<typeof loader>();
+  console.log(properties);
 
   return (
-    <div className="w-full h-without-nav-fixed bg-primary text-white">
+    <div className="w-full h-without-nav-auto bg-primary text-white">
       <main className="px-4 h-full flex flex-col pt-6">
         <h1 className="text-4xl font-bold text-center pb-4">Properties</h1>
-        <div className="properties-list w-full">
-          <PropertyCard {...property} />
+        <div className="properties-list w-full flex flex-col gap-2 pb-2">
+          {properties.properties.map((property) => (
+            <PropertyCard key={property.id} {...property} />
+          ))}
         </div>
       </main>
     </div>
