9b3609a58524699cdb02830168bf4ed417e2026f
Victor Tran <victor-likes-coding@tempestas.io>
2024-01-14 05:37:47 -0500

    Refactor user authentication data types

--- app/models/user.d.ts
+++ app/models/user.d.ts
@@ -1,4 +1,10 @@
-export type userAuthData = {
+export interface userAuthData {
+  [key: string]: FormDataEntryValue | undefined | boolean;
   email: string;
   password: string;
-};
+  adminCode?: string;
+}
+
+export interface elevatedAuthData extends userAuthData {
+  admin?: boolean;
+}
