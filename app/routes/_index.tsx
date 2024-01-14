cdff829e28c909221d4247ed04cff286d9b6a17f
Victor Tran <victor-likes-coding@tempestas.io>
2024-01-14 01:01:14 -0500

    Add Navbar component to index page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
 import Button from "~/components/button";
+import Navbar from "~/components/navbar";
 
 export const meta: MetaFunction = () => {
   return [
@@ -11,17 +12,20 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="w-full h-without-nav-fixed bg-primary">
-      <main className="px-16 h-full flex justify-center align-middle flex-col text-center">
-        <h1 className="text-3xl mb-5">
-          Welcome to{" "}
-          <span className="font-bold text-secondary">Sustainable Spaces</span>
-        </h1>
-        <Button text="Get Started" link={true} to="/signup" />
-        <Link to={"/about"} className="text-sm mt-2 underline">
-          Learn more
-        </Link>
-      </main>
-    </div>
+    <>
+      <Navbar />
+      <div className="w-full h-without-nav-fixed bg-primary">
+        <main className="px-16 h-full flex justify-center align-middle flex-col text-center">
+          <h1 className="text-3xl mb-5">
+            Welcome to{" "}
+            <span className="font-bold text-secondary">Sustainable Spaces</span>
+          </h1>
+          <Button text="Get Started" link={true} to="/signup" />
+          <Link to={"/about"} className="text-sm mt-2 underline">
+            Learn more
+          </Link>
+        </main>
+      </div>
+    </>
   );
 }
