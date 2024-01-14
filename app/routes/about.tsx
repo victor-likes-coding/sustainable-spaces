ff2353bca17240b77b820be7395358e5f08bd167
Victor Tran <victor-likes-coding@tempestas.io>
2024-01-14 01:02:26 -0500

    Add Navbar component to About page

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -2,35 +2,41 @@
 // import { Link } from "@remix-run/react";
 
 import Button from "~/components/button";
+import Navbar from "~/components/navbar";
 
 export default function About() {
   return (
-    <div className="w-full h-without-nav-auto bg-primary">
-      <main className="px-8 h-full flex flex-col gap-4 pb-8">
-        <h2 className="text-lg font-bold pt-6 text-center">Our Company Goal</h2>
-        <p className="text-md">
-          At{" "}
-          <span className="text-secondary font-bold">Sustainable Spaces</span>,
-          we are dedicated to creating affordable rental solutions and
-          connecting homeowners with those in need of housing. Our comprehensive
-          approach simplifies real estate investing, offering vital services
-          such as tenant screening, background checks, efficient rent
-          collection, and facilitating the buying and selling of properties
-          within our community.
-        </p>
+    <>
+      <Navbar />
+      <div className="w-full h-without-nav-auto bg-primary">
+        <main className="px-8 h-full flex flex-col gap-4 pb-8">
+          <h2 className="text-lg font-bold pt-6 text-center">
+            Our Company Goal
+          </h2>
+          <p className="text-md">
+            At{" "}
+            <span className="text-secondary font-bold">Sustainable Spaces</span>
+            , we are dedicated to creating affordable rental solutions and
+            connecting homeowners with those in need of housing. Our
+            comprehensive approach simplifies real estate investing, offering
+            vital services such as tenant screening, background checks,
+            efficient rent collection, and facilitating the buying and selling
+            of properties within our community.
+          </p>
 
-        <p>
-          To make our low-cost rental options a reality, we utilize a unique
-          crowdfunding approach, pooling resources from our users. Coupled with
-          creative financing and other innovative tools, we can directly
-          purchase properties from homeowners, ensuring affordability. While
-          homeowners are welcome to list their properties on our platform, they
-          should be aware that they will be competing with the attractive prices
-          we are able to offer through our direct purchase strategy.
-        </p>
+          <p>
+            To make our low-cost rental options a reality, we utilize a unique
+            crowdfunding approach, pooling resources from our users. Coupled
+            with creative financing and other innovative tools, we can directly
+            purchase properties from homeowners, ensuring affordability. While
+            homeowners are welcome to list their properties on our platform,
+            they should be aware that they will be competing with the attractive
+            prices we are able to offer through our direct purchase strategy.
+          </p>
 
-        <Button text="Donate Today" className="mt-2" />
-      </main>
-    </div>
+          <Button text="Donate Today" className="mt-2" />
+        </main>
+      </div>
+    </>
   );
 }
