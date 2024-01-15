6221aa315670f5a83e4b3d48cbe893e183fa17db
Victor Tran <victor-likes-coding@tempestas.io>
2024-01-14 20:48:52 -0500

    Refactor User class and UserService

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,22 +1,23 @@
-import { elevatedAuthData, userAuthData } from "./user.d";
+import {
+  authUser,
+  databaseUser,
+  elevatedAuthData,
+  userAuthData,
+} from "./user.d";
 // import { Property } from "./property";
-import { Bank } from "./bank";
+// import { Bank } from "./bank";
 import { validateAuth } from "~/utils/helper";
 import { PasswordService } from "./password";
+import { db } from "~/utils/db.server";
 
-interface iUser {
-  id?: string;
-  email?: string;
-  bank?: Bank;
-}
+export class User implements authUser {
+  [key: string]: number | string | undefined;
+  email: string;
+  id: number;
+  firstName?: string | undefined;
+  lastName?: string | undefined;
 
-export class User implements iUser {
-  bank?: Bank;
-  id?: string;
-  email?: string;
-
-  constructor({ bank, id, email }: iUser) {
-    this.bank = bank;
+  constructor({ id, email }: databaseUser) {
     this.id = id;
     this.email = email;
   }
@@ -26,15 +27,24 @@ export abstract class UserService {
   public static async createUser(data: userAuthData | elevatedAuthData) {
     // validate userAuthData
     if (validateAuth(data)) {
-      // hash password
-      const hash = await PasswordService.hashPassword({
-        password: data.password,
-        saltRounds: 10,
-      });
-      // store data into database
-      const user = UserService.createModel(data, hash);
-
       // return newly created user
+      try {
+        const newUser = await db.$transaction(async (trx) => {
+          // hash password
+          const hash = await PasswordService.hashPassword({
+            password: data.password,
+            saltRounds: 10,
+          });
+          // store data into database
+          const confidentialUser = UserService.createModel(data, hash);
+          const user = await trx.user.create({ data: confidentialUser });
+          return new User(user);
+        });
+
+        return newUser;
+      } catch (error) {
+        throw new Error("error creating user");
+      }
     }
   }
 
@@ -45,4 +55,4 @@ export abstract class UserService {
     };
   }
 }
-export type { userAuthData, elevatedAuthData };
+export type { userAuthData, elevatedAuthData, databaseUser, authUser };
