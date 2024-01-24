b70e0702cd240e1949eb46cffcc1a28b6340ecf0
Victor Tran <victor-likes-coding@tempestas.io>
2024-01-24 16:53:50 -0500

    Add error handling for user creation

--- app/models/user.ts
+++ app/models/user.ts
@@ -9,6 +9,12 @@ import {
 import { validateAuth } from "~/utils/helper";
 import { PasswordService } from "./password";
 import { db } from "~/utils/db.server";
+import {
+  DatabaseCreationServiceError,
+  ExistingUserError,
+  HashingPasswordError,
+  UserNotFoundError,
+} from "~/utils/errors";
 
 export class User implements authUser {
   [key: string]: number | string | undefined;
@@ -24,27 +30,65 @@ export class User implements authUser {
 }
 
 export abstract class UserService {
-  public static async createUser(data: userAuthData | elevatedAuthData) {
+  public static async checkForExistingUser(email: string) {
+    const user = await db.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (user) throw new ExistingUserError();
+  }
+
+  /**
+   * The register function will validate the data sent to it and check for existing users. Once the checks are done, it will create a new user in the database and return it.
+   * @param data - an object containing string properties `email` and `password` or `adminCode`
+   * @returns {User}
+   */
+
+  public static async register(data: userAuthData | elevatedAuthData) {
     // validate userAuthData
-    if (validateAuth(data)) {
-      // return newly created user
-      try {
-        const newUser = await db.$transaction(async (trx) => {
-          // hash password
+    validateAuth(data); // this will throw an error or simply do nothing // should be caught when calling this function a try/catch where this method was called.
+
+    // check if user already exists
+    await UserService.checkForExistingUser(data.email);
+    const user = await UserService.createUser(data);
+    return user;
+  }
+
+  public static async getUserById(id: number | string) {
+    const user = await db.user.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    if (!user) throw new UserNotFoundError("User not found");
+    return new User(user);
+  }
+
+  public static async createUser(data: userAuthData | elevatedAuthData) {
+    // return newly created user
+    try {
+      const newUser = await db.$transaction(async (trx) => {
+        // hash password
+        try {
           const hash = await PasswordService.hashPassword({
             password: data.password,
             saltRounds: 10,
           });
+
           // store data into database
           const confidentialUser = UserService.createModel(data, hash);
           const user = await trx.user.create({ data: confidentialUser });
           return new User(user);
-        });
+        } catch (error) {
+          throw new HashingPasswordError();
+        }
+      });
 
-        return newUser;
-      } catch (error) {
-        throw new Error("error creating user");
-      }
+      return newUser;
+    } catch (error) {
+      if (error instanceof HashingPasswordError) throw error;
+      throw new DatabaseCreationServiceError();
     }
   }
 
