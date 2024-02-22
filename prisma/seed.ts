772f6a84c5e0e94a1b62aea49464123135743177
Victor Tran <victor-likes-coding@tempestas.io>
2024-02-22 03:51:37 -0500

    Commented out property seeding code

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,11 +9,11 @@ async function seed() {
     })
   );
 
-  await Promise.all(
-    getProperties().map((property) => {
-      return db.property.create({ data: property });
-    })
-  );
+  // await Promise.all(
+  //   getProperties().map((property) => {
+  //     return db.property.create({ data: property });
+  //   })
+  // );
 }
 
 seed();
@@ -31,82 +31,8 @@ function getUsers() {
   ];
 }
 
-function getProperties() {
-  return [
-    {
-      address: "123 Main Street",
-      city: "San Diego",
-      state: "CA",
-      zip: 92101,
-      price: 1000000,
-      beds: 3,
-      baths: 2,
-      sqft: 2000,
-      tax: 0,
-      hoa: 0,
-      insurance: 0,
-      rent: 0,
-      payment: 0,
-      garage: 2,
-      ownerId: 1,
-      tenantId: null,
-      likes: [],
-      likesCount: 0,
-      longitude: 0,
-      latitude: 0,
-      description: "This is a description",
-      allowRentOption: false,
-      year: 1935,
-    },
-    {
-      address: "456 Main Street",
-      city: "San Diego",
-      state: "CA",
-      zip: 92101,
-      price: 2000000,
-      beds: 3,
-      baths: 2,
-      sqft: 2000,
-      tax: 0,
-      hoa: 0,
-      insurance: 0,
-      rent: 0,
-      payment: 0,
-      garage: 2,
-      ownerId: 1,
-      tenantId: null,
-      likes: [],
-      likesCount: 0,
-      longitude: 0,
-      latitude: 0,
-      description: "This is a description",
-      allowRentOption: false,
-      year: 1935,
-    },
-    {
-      address: "789 Main Street",
-      city: "San Diego",
-      state: "CA",
-      zip: 92101,
-      price: 3000000,
-      beds: 3,
-      baths: 2,
-      sqft: 2000,
-      tax: 0,
-      hoa: 0,
-      insurance: 0,
-      rent: 0,
-      payment: 0,
-      garage: 2,
-      ownerId: 1,
-      tenantId: null,
-      likes: [],
-      likesCount: 0,
-      longitude: 0,
-      latitude: 0,
-      description: "This is a description",
-      allowRentOption: false,
-      year: 1935,
-    },
-  ];
-}
+// function getProperties() {
+//   return [
+
+//   ];
+// }
