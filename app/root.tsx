000532fdb1b51500c68e5bb8c9c3fedc69fccc6d
Victor Tran <victor-likes-coding@tempestas.io>
2024-03-12 19:33:16 -0400

    Add NextUIProvider to root component

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { NextUIProvider } from "@nextui-org/react";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: tailwindStyles },
@@ -24,45 +25,47 @@ export default function App() {
         <Links />
       </head>
       <body className="font-default">
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-        <footer className="bg-dark text-white p-4 text-[.6rem] px-8 relative z-100 bottom-0 left-0">
-          <nav className="flex justify-between flex-wrap">
-            <div className="col-wrapper flex flex-col gap-2">
-              <div className="underline">Useful Links</div>
-              <Link to={{ pathname: "/" }}>Home</Link>
-              <Link to="/about">About</Link>
-              <Link to="/services">Services</Link>
-              <Link to="/privacy">
-                Privacy <span className="hidden">Policy</span>
-              </Link>
-              <Link to="/tos">
-                Terms <span className="hidden">and Conditions</span>
-              </Link>
-            </div>
-            <div className="col-wrapper flex flex-col gap-2">
-              <div className="underline">Community</div>
-              <Link to="/help">Help</Link>
-              <Link to="/properties">Properties</Link>
-              <Link to="/forum">Forum</Link>
-              <Link to="/support">Support</Link>
-            </div>
+        <NextUIProvider>
+          <Outlet />
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+          <footer className="bg-dark text-white p-4 text-[.6rem] px-8 relative z-100 bottom-0 left-0">
+            <nav className="flex justify-between flex-wrap">
+              <div className="col-wrapper flex flex-col gap-2">
+                <div className="underline">Useful Links</div>
+                <Link to={{ pathname: "/" }}>Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/services">Services</Link>
+                <Link to="/privacy">
+                  Privacy <span className="hidden">Policy</span>
+                </Link>
+                <Link to="/tos">
+                  Terms <span className="hidden">and Conditions</span>
+                </Link>
+              </div>
+              <div className="col-wrapper flex flex-col gap-2">
+                <div className="underline">Community</div>
+                <Link to="/help">Help</Link>
+                <Link to="/properties">Properties</Link>
+                <Link to="/forum">Forum</Link>
+                <Link to="/support">Support</Link>
+              </div>
 
-            <div className="col-wrapper flex flex-col gap-2">
-              <div className="underline">Contact</div>
-              <Link to="mailto:some-email@email.com" target="__blank">
-                some-email@email.com
-              </Link>
-              <Link to="tel:+1-800-123-4567" target="__blank">
-                +1 (800) 123-4567
-              </Link>
-              <div>New York, NY 10012, US</div>
-            </div>
-            <div className="flex w-full justify-around"></div>
-          </nav>
-        </footer>
+              <div className="col-wrapper flex flex-col gap-2">
+                <div className="underline">Contact</div>
+                <Link to="mailto:some-email@email.com" target="__blank">
+                  some-email@email.com
+                </Link>
+                <Link to="tel:+1-800-123-4567" target="__blank">
+                  +1 (800) 123-4567
+                </Link>
+                <div>New York, NY 10012, US</div>
+              </div>
+              <div className="flex w-full justify-around"></div>
+            </nav>
+          </footer>
+        </NextUIProvider>
       </body>
     </html>
   );
