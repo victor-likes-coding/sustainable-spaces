4ba02e689b4ce752e725032c17ceeea2bd40e8d8
Victor Tran <victor-likes-coding@tempestas.io>
2024-03-14 16:03:23 -0400

    Refactor property model and import AddressData

--- app/models/property.d.ts
+++ app/models/property.d.ts
@@ -1,26 +1,6 @@
-import { User } from "./user";
-import { Property } from "./property";
+import { AddressData } from "./property";
 
-export interface DatabaseSafeProperty
-  extends Partial<DefaultDatabaseProperties>,
-    Address,
-    Partial<ZillowPropertyData>,
-    PropertyFees {
-  paymentType: "rent" | "sell";
-  garage: number;
-  owner?: string | User;
-  tenant?: string | User;
-  likes: number[];
-  likesCount: number;
-}
-
-export interface Address {
-  city: string;
-  state: string;
-  streetAddress: string;
-  zipcode: string;
-}
-
+// props from zillow scrape
 export interface ZillowPropertyData {
   zpid: number;
   yearBuilt: number;
@@ -35,48 +15,11 @@ export interface ZillowPropertyData {
   description: string;
   bedrooms: number;
   bathrooms: number;
-  address: Record<keyof Address, string>;
+  address: Record<keyof AddressData, string>;
   timestamp?: string;
   tax?: number;
   annualHomeownersInsurance: number;
   zillowLink?: string;
   price: number;
   garage: number;
-}
-
-export interface DefaultDatabaseProperties {
-  id: number;
-  updated: Date;
-  created: Date;
-}
-
-export interface BasicPropertyData extends ZillowPropertyData {
-  fees: PropertyFees;
-}
-
-export interface PropertyFees {
-  management: number;
-  capex: number;
-  vacancy: number;
-  hoa?: number | null;
-}
-
-export interface FullPropertyData
-  extends BasicPropertyData,
-    DefaultDatabaseProperties {
-  price: number;
-  paymentType: "rent" | "sell";
-  garage: number;
-  owner: User | string;
-  tenant: User | string;
-  likes: number[];
-  likesCount: number;
-}
-
-export abstract class PropertyService {
-  static getProperties(): Promise<Property[]>;
-  abstract getProperty(id: number): Promise<Property>;
-  abstract createProperty(property: Property): Promise<Property>;
-  abstract updateProperty(property: Property): Promise<Property>;
-  abstract deleteProperty(id: number): Promise<void>;
 }
