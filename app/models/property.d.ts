2c2322f0441616e823d4ded8e72dccba5226360e
Victor Tran <victor-likes-coding@tempestas.io>
2024-03-21 01:35:39 -0400

    Update types in property.d.ts file

--- app/models/property.d.ts
+++ app/models/property.d.ts
@@ -2,24 +2,47 @@ import { AddressData } from "./property";
 
 // props from zillow scrape
 export interface ZillowPropertyData {
-  zpid: number;
-  yearBuilt: number;
+  zpid: number | string;
+  yearBuilt: number | string;
   parcelId: string;
-  lotSize: number;
+  lotSize: number | string;
   lotAreaUnits: string;
-  livingArea: number;
+  livingArea: number | string;
   livingAreaUnits: string;
-  latitude: number;
-  longitude: number;
+  latitude: number | string;
+  longitude: number | string;
   homeType: string;
   description: string;
-  bedrooms: number;
-  bathrooms: number;
+  bedrooms: number | string;
+  bathrooms: number | string;
   address: Record<keyof AddressData, string>;
   timestamp?: string;
-  tax?: number;
-  annualHomeownersInsurance: number;
+  tax?: number | string;
+  annualHomeownersInsurance: number | string;
   zillowLink?: string;
-  price: number;
-  garage: number;
+  price: number | string;
+  garage: number | string;
+}
+
+export interface PropertyFormData {
+  zpid: string;
+  yearBuilt: string;
+  parcelId: string;
+  lotSize: string;
+  lotAreaUnits: string;
+  livingArea: string;
+  livingAreaUnits: string;
+  latitude: string;
+  longitude: string;
+  homeType: string;
+  description: string;
+  bedrooms: string;
+  bathrooms: string;
+  address: AddressData;
+  timestamp?: string;
+  tax?: string;
+  annualHomeownersInsurance: string;
+  zillowLink?: string;
+  price: string;
+  garage: string;
 }
