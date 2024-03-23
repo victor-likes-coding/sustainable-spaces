1bc2774998a071e07c895822ba91ed565bdff2af
Victor <victor-likes-coding@tempestas.io>
2024-03-23 01:17:24 -0400

    Add ErrorBoundary component to handle route errors

--- app/root.tsx
+++ app/root.tsx
@@ -8,6 +8,7 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useRouteError,
 } from "@remix-run/react";
 import { NextUIProvider } from "@nextui-org/react";
 
@@ -66,6 +67,33 @@ export default function App() {
             </nav>
           </footer>
         </NextUIProvider>
+      </body>
+    </html>
+  );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+  console.log(error);
+  let message: string = "";
+  if (
+    error instanceof Error &&
+    error.name === "PrismaClientKnownRequestError"
+  ) {
+    message = "You need an internet connection to be able to view this page.";
+  }
+  return (
+    <html lang="en">
+      <head>
+        <title>Oh no!</title>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <h1>Oh no!</h1>
+        <p>Something went wrong.</p>
+        <p>{message}</p>
+        <Scripts />
       </body>
     </html>
   );
