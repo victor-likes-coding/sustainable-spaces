74ddc73991754687982f8fcba90a631e5ece0ee3
Victor <victor-likes-coding@tempestas.io>
2024-03-26 16:29:35 -0400

    Add Loader component to PropertyCard

--- app/components/propertycard.tsx
+++ app/components/propertycard.tsx
@@ -1,6 +1,7 @@
 import { Image } from "@nextui-org/react";
 import { Link } from "@remix-run/react";
 import { PropertyDataStructure } from "~/models/property";
+import Loader from "./Loader";
 
 const PropertyCard = (property?: PropertyDataStructure) => {
   if (!property) {
@@ -18,45 +19,57 @@ const PropertyCard = (property?: PropertyDataStructure) => {
   } = property;
 
   return (
-    <div className="card-container w-full h-auto text-sm rounded-md border-secondary relative z-0 text-white">
-      <div className="card-image-wrapper w-full pb-2">
-        <Image
-          src={
-            images && images.length > 0
-              ? images[0].url
-              : "https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
-          }
-        />
-      </div>
-      <div className="card-content w-full px-2">
-        <div className="card-title w-full flex justify-between pt-1 pr-3">
-          <div className="left font-bold">
-            {streetAddress} | {state}
-          </div>
-          <div className="right font-semibold">
-            {price
-              .toLocaleString("en-US", {
-                style: "currency",
-                currency: "USD",
-              })
-              .replace(/\.\d+/, "")}
-          </div>
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+    <>
+      <Link
+        to={`/property/${id}`}
+        className="card-container w-full h-auto text-sm rounded-md border-secondary relative z-0 text-white cursor-pointer"
+      >
+        <div className="card-image-wrapper w-full pb-2">
+          <Image
+            src={
+              images && images.length > 0
+                ? images[0].url
+                : "https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
+            }
+          />
         </div>
-        <Link to={{ pathname: `/property/${id}` }}>
+        <div className="card-content w-full px-2">
+          <div className="card-title w-full flex justify-between pt-1 pr-3">
+            <div className="left font-bold">
+              {streetAddress} | {state}
+            </div>
+            <div className="right font-semibold">
+              {price
+                .toLocaleString("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                })
+                .replace(/\.\d+/, "")}
+            </div>
+          </div>
           <div className="card-description w-full h-8 flex justify-between items-center">
             <div className="left text-xs">
               {bedrooms} beds • {bathrooms} baths • {livingArea} sqft
             </div>
           </div>
-        </Link>
-        <Link
-          to={{ pathname: `/property/${id}/bid` }}
-          className="absolute bottom-2 right-2 px-8 bg-green-700 text-white rounded-md"
-        >
-          Bid
-        </Link>
-      </div>
-    </div>
+          <Link
+            onClick={(e) => e.stopPropagation()}
+            to={{ pathname: `/property/${id}/bid` }}
+            className="absolute bottom-2 right-2 px-8 bg-green-700 text-white rounded-md"
+          >
+            Bid
+          </Link>
+        </div>
+      </Link>
+      {navigate.state === "loading" && (
+        <Loader
+          labelColor="primary"
+          color="primary"
+          text="Navigating to property"
+        />
+      )}
+    </>
   );
 };
 
