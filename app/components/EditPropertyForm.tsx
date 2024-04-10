81e4bf2725f9daf1ef12e4e8e1ed1c3d83905b95
Victor <victor-likes-coding@tempestas.io>
2024-04-09 20:53:17 -0400

    Update property edit page with new form and error handling

---
+++ app/components/EditPropertyForm.tsx
@@ -0,0 +1,236 @@
+import BasicForm from "./BasicForm";
+import { ChangeEvent, useState } from "react";
+import { Button } from "@nextui-org/react";
+import { useSubmit } from "@remix-run/react";
+import AddressInputFields from "./AddressInputFields";
+import PropertyCharacteristicsInputFields from "./PropertyCharacteristicsInputFields";
+import {
+  handleFileRemoval,
+  handleFileUpload,
+  prepareFormData,
+} from "~/utils/helper";
+import CloseSVG from "./svg/Close";
+import PlusSVG from "./svg/Plus";
+import { PropertyDataStructure } from "~/models/property.zod";
+
+type Props = {
+  className?: string;
+  property: PropertyDataStructure;
+  setProperty: React.Dispatch<React.SetStateAction<PropertyDataStructure>>;
+  method: "post" | "get" | "put" | "delete";
+  action?: string;
+};
+
+export default function EditPropertyForm({
+  property,
+  setProperty,
+  ...props
+}: Props) {
+  const submit = useSubmit();
+
+  const [fileData, setFileData] = useState<FileList | null>(null);
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = prepareFormData(property, fileData); // prepares form data to upload
+
+    submit(formData, { encType: "multipart/form-data", method: "post" });
+  };
+
+  const handleAddressChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setProperty((prevProperty) => ({
+      ...prevProperty,
+      address: {
+        ...prevProperty.address,
+        [event.target.name]: event.target.value,
+      },
+    }));
+  };
+
+  const handleChange = (
+    event: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    setProperty((prevProperty) => ({
+      ...prevProperty,
+      [event.target.name]:
+        event.target.tagName === "INPUT"
+          ? event.target.value
+          : (event.target.value as "rent" | "sell"),
+    }));
+  };
+
+  const handleLocalImageRemoval = (index: number) => {
+    const dataTransfer = handleFileRemoval(fileData, index);
+    if (dataTransfer !== null && dataTransfer?.files?.length > 0)
+      setFileData(dataTransfer.files);
+  };
+
+  const handleLocalImageUpload = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const dataTransfer = handleFileUpload(event, fileData);
+    setFileData(dataTransfer.files);
+  };
+
+  const markImageAsInactive = (id: number) => {
+    // delete image
+    const image = images?.filter((image) => image.id === id)[0];
+    if (!image) return;
+
+    image.active = false;
+
+    setProperty((prevProperty) => ({
+      ...prevProperty,
+      images: [...prevProperty.images, image],
+    }));
+  };
+
+  const {
+    bedrooms,
+    bathrooms,
+    description,
+    lotSize,
+    livingArea,
+    yearBuilt,
+    purchaseMethod,
+    price,
+    images,
+    garage,
+    ...rest
+  } = property;
+
+  const {
+    address,
+    fees,
+    longitude,
+    latitude,
+    homeType,
+    tax,
+    annualHomeownersInsurance,
+    ...hidden
+  } = rest;
+  const feeFields = {
+    ...fees,
+    tax,
+    annualHomeownersInsurance,
+  };
+
+  const propertyCharacteristicsData = {
+    bedrooms,
+    bathrooms,
+    description,
+    lotSize,
+    livingArea,
+    yearBuilt,
+    purchaseMethod,
+    price,
+    garage,
+    longitude,
+    latitude,
+    homeType,
+  };
+
+  const imagesAreDefined =
+    images && images.filter((image) => image.active).length > 0;
+  const fileDataIsDefined = fileData && fileData.length > 0;
+
+  return (
+    <BasicForm {...props} onSubmit={handleSubmit}>
+      {/* need to show images */}
+      {/* these are going to be horizontal images / square images / need to enforce aspect ratio when adding images in the future! */}
+      {/* show images */}
+      <div className="image-container flex overflow-x-scroll mb-4">
+        {imagesAreDefined && (
+          <>
+            {images
+              .filter((image) => image.active)
+              .map((image) => (
+                <div
+                  key={image.id}
+                  className="image-container__wrapper flex-shrink-0 flex-grow-0 basis-72 relative"
+                >
+                  <img
+                    src={image.url}
+                    alt="property"
+                    className="image-container__image"
+                  />
+
+                  <CloseSVG
+                    onClick={() => {
+                      markImageAsInactive(image.id);
+                    }}
+                  />
+                </div>
+              ))}
+          </>
+        )}
+        {/* this is where the file uploads will appear */}
+        {fileDataIsDefined && (
+          <>
+            {Array.from(fileData).map((file, index) => (
+              <div
+                key={file.name}
+                className="image-container__wrapper flex-shrink-0 flex-grow-0 basis-72 relative"
+              >
+                <img
+                  src={URL.createObjectURL(file)}
+                  alt="property"
+                  className="image-container__image"
+                />
+                <CloseSVG onClick={() => handleLocalImageRemoval(index)} />
+              </div>
+            ))}
+          </>
+        )}
+        {/* this is where the upload component will appear */}
+        <div
+          className={
+            "image-container__upload flex-shrink-0 flex-grow-0 basis-72 relative bg-slate-600 flex items-center justify-center"
+          }
+        >
+          <button
+            type="button"
+            className={"flex justify-center items-center "
+              .concat(
+                imagesAreDefined ? "w-full h-full" : "w-[288px] h-[192px]"
+              )
+              .trim()}
+            onClick={() => {
+              const fileInput = document.getElementById(
+                "file"
+              ) as HTMLInputElement;
+              fileInput.click();
+            }}
+          >
+            <PlusSVG
+              style={{ svg: "flex items-center justify-center" }}
+              size={5}
+            />
+            <input
+              type="file"
+              className="hidden-input"
+              id="file"
+              name="file"
+              multiple
+              accept="image/*"
+              onChange={handleLocalImageUpload}
+            />
+          </button>
+        </div>
+        {/* it'll be a horizontal upload with a different design */}
+      </div>
+      <AddressInputFields address={address} setProperty={handleAddressChange} />
+      <PropertyCharacteristicsInputFields
+        data={propertyCharacteristicsData}
+        setProperty={handleChange}
+      />
+      <Button
+        type="submit"
+        className=" bg-custom-secondary text-xs font-bold text-white mb-4"
+      >
+        Update
+      </Button>
+    </BasicForm>
+  );
+}
--- app/routes/property.$propertyId_.edit.tsx
+++ app/routes/property.$propertyId_.edit.tsx
@@ -1,29 +1,116 @@
 // import { Form, Link } from "@remix-run/react";
 
-import { LoaderFunctionArgs, json } from "@remix-run/node";
+import {
+  ActionFunctionArgs,
+  json,
+  LoaderFunctionArgs,
+  redirect,
+} from "@remix-run/node";
+import {
+  Links,
+  Meta,
+  Scripts,
+  useLoaderData,
+  useRouteError,
+} from "@remix-run/react";
+import { useState } from "react";
+import EditPropertyForm from "~/components/EditPropertyForm";
+import { ImageService } from "~/models/Image";
+import { PropertyService } from "~/models/property";
+import { PropertyDataStructure } from "~/models/property.zod";
+import { editFormPermissionSelect } from "~/types/property.select";
+import { UnauthorizedMutationRequestError } from "~/utils/errors";
 import { validatePropertyOwner } from "~/utils/helper";
 
 // import Button from "~/components/button";
 
+export const action = async ({ request, params }: ActionFunctionArgs) => {
+  // await validatePropertyOwner(params, request); // validates the property owner
+  const req = request.clone();
+  const formData = await req.formData();
+
+  const files = formData.getAll("file"); // these are the new images to upload
+
+  // const imageUrls = await uploadImages(files);
+
+  const data = formData.get("data");
+
+  if (!data) {
+    throw Error("No data found in form");
+  }
+
+  const parsedData = PropertyService.transformToPropertyData(data as string);
+  // validate data
+  // flatten the data
+
+  const property = await PropertyService.updateProperty(propertyData);
+  ImageService.deactivateImages(images);
+
+  return "";
+};
+76;
+
 export const loader = async ({ params, request }: LoaderFunctionArgs) => {
+  let property: Partial<PropertyDataStructure> = {};
   try {
-    await validatePropertyOwner(params, request);
+    const { property: validatedProperty } = await validatePropertyOwner(
+      params,
+      request,
+      editFormPermissionSelect
+    );
+    property = validatedProperty;
   } catch (err) {
     if (err instanceof UnauthorizedMutationRequestError) {
-      console.log("redirecting");
       throw redirect(`/property/${params.propertyId}?error=unauthorized`);
     }
   }
-
-  const { property } = await validateAndRetrieveProperty(params, request);
 
   return json(property);
 };
 
 export default function Property() {
+  const property: PropertyDataStructure = useLoaderData<typeof loader>();
+  // remove tenantId, zpid, likes, likesCount, updated, created,
+
+  const [propertyData, setPropertyData] =
+    useState<PropertyDataStructure>(property);
+
+  // show images
+  // show button next to it to add photos
+  // each image will have an x on it to be removed from list of images
+  // image dele te should only be finallized after the form is submitted
+  // if user attaches a new image set the image like how we did in the add form
+  // except this time we can preview the image before uploading it
+  // final changes are processed once saved
+
   return (
-    <div className="w-full h-without-nav-fixed bg-primary text-white">
-      <main className="px-4 h-full flex flex-col pt-6">edit</main>
+    <div className="w-full h-without-nav-auto bg-custom-primary text-white">
+      <main className="px-4 h-full flex flex-col pt-6">
+        <EditPropertyForm
+          property={propertyData}
+          method="post"
+          setProperty={setPropertyData}
+        />
+      </main>
     </div>
+  );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+  console.log(error);
+  return (
+    <html lang="en">
+      <head>
+        <title>Oh no!</title>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        ¬<h1>Oh no!</h1>
+        <p>Something went wrong.</p>
+        <Scripts />
+      </body>
+    </html>
   );
 }
