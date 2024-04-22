9d41a3fe6fba3466110d427fee5afebee3f62631
Victor <victor-likes-coding@tempestas.io>
2024-04-22 17:12:14 -0400

    Extract property cards into it's own component for reusability

---
+++ app/components/PropertyCards.tsx
@@ -0,0 +1,26 @@
+import PropertyCard from "~/components/propertycard";
+import { PropertyWithImages } from "~/types/property.new";
+
+interface Props {
+  properties: PropertyWithImages[];
+}
+
+export function PropertyCards({ properties }: Props) {
+  return (
+    <div className="properties-list w-full flex flex-col gap-4 pb-4 px-3">
+      {properties?.map((property) => {
+        return (
+          <PropertyCard
+            key={property.id}
+            property={property as unknown as PropertyWithImages}
+          />
+        );
+      })}
+      {properties?.length === 0 && (
+        <div className="text-center text-xl font-bold">
+          No properties to display
+        </div>
+      )}
+    </div>
+  );
+}
