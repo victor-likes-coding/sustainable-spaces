a829c862b85570f7b5ff69c6fe5e709e505a7302
Victor <victor-likes-coding@tempestas.io>
2024-08-22 04:28:32 -0400

    update the route with some comments and a console.log

--- app/routes/getPropertyData.ts
+++ app/routes/getPropertyData.ts
@@ -2,4 +2,8 @@ import { ActionFunction, ActionFunctionArgs } from '@remix-run/node';
 
 export const action: ActionFunction = async ({
   request,
-}: ActionFunctionArgs) => {};
+}: ActionFunctionArgs) => {
+  // property data -> server -> checks if property exists -> returns data to client
+  console.log(request.body);
+  // if doesn't exist -> scrape zillow -> transform data -> return to client
+};
